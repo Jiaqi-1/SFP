997b3583c3bfa6c2386f9412bb7c2766d3b3f36a
Jiaqi-1 <25080268@imail.sunway.edu.my>
2025-07-22 02:29:35 +0000

    done for day 2

---
+++ Part2/ex14.py
@@ -0,0 +1,6 @@
+# List of countries
+countries = ['Malaysia', 'Japan', 'Armenia', 'Brazil', 'Australia']
+
+# Create a for loop that goes through the list and prints each country with a message
+for country in countries:
+    print(f"Come to visit {country}!")
---
+++ Part2/ex16.py
@@ -0,0 +1,10 @@
+# TODO:
+# 1. Ask the user for their name
+# 2. Ask the user for their favourite food
+# 3. Print a message like:
+# "Hi Marvin, I heard you love cendol!"
+
+name = input("What is your name?Marvin ")
+food = input("What is your favourite food?cendol ")
+
+print(f"Hi {name}, I heard you love {food}cendol!")
